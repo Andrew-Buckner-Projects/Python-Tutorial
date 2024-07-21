3244129acb138b693b33c9814faaa5bd028cdbb0
Andrew Buckner <buckner.andrew@gmail.com>
2024-07-21 18:30:33 +0200

    part 2

--- python_sandbox_starter/functions.py
+++ python_sandbox_starter/functions.py
@@ -1,5 +1,25 @@
 # A function is a block of code which only runs when it is called. In Python, we do not use parentheses and curly brackets, we use indentation with tabs or spaces
 
+#creat a function
+def sayHello(name = 'Forgot to enter'):
+    print(f'Hello {name}')
+
+sayHello('John Doe')
+sayHello()
+
+#return Values
+def getSum(num1, num2):
+    total = num1 + num2
+    return total
+
+num = getSum(3,4)
+print(num)
+
+# Lambda function
+
+getSum = lambda num1, num2 : num1 + num2
+print(getSum(10,3))
+
 
 
 # A lambda function is a small anonymous function.
